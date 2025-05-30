73338e37dc04807f79cf0ef84ae2ba244d9b8388
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2025-05-29 23:32:46 -0700

    changed name of content.js to usernameCheck.js

--- background.js
+++ background.js
@@ -1,1 +1,2 @@
 // handles API calls
+// Will run an API call at intervals to check for updates, as well as checking the database for any Antis.
---
+++ usernameCheck.js
@@ -0,0 +1,1 @@
+// checks usernames on twitter and sees if any usernames on the current page matches anyone that is in the Anti database.
