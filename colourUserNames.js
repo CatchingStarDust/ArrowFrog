eecfbc4b2fdd87cc65b6626d29813af607b33235
CatchingStarDust <120714158+CatchingStarDust@users.noreply.github.com>
2025-10-22 19:29:59 -0700

    established the red highlight colour

--- colourUserNames.js
+++ colourUserNames.js
@@ -1,1 +1,6 @@
 // the actual function that colours the usernames of twitter users
+const redHighlight = #FF0000;
+
+function colourUsernames() {
+    document.getElementById("WHATEVER_THE_ELEMENT_IS_FOR_TWITTER_USERNAMES").style.color = "redHighlight";
+}
